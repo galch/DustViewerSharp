cb6f5231ffb7a4d6db5b578a0e315b25353a0b4f
Galch <jaehun.han@navercorp.com>
2016-09-15 15:13:27 +0900

    font change to D2_Coding, and control position adjustment

--- DustSensorViewer/Form1.Designer.cs
+++ DustSensorViewer/Form1.Designer.cs
@@ -59,7 +59,7 @@
             this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
             this.splitContainer1.IsSplitterFixed = true;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -71,19 +71,20 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
-            this.splitContainer1.Size = new System.Drawing.Size(1142, 722);
-            this.splitContainer1.SplitterDistance = 60;
+            this.splitContainer1.Size = new System.Drawing.Size(937, 906);
+            this.splitContainer1.SplitterDistance = 38;
             this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 0;
             // 
             // button_con
             // 
             this.button_con.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_con.Font = new System.Drawing.Font("D2Coding", 9.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button_con.ForeColor = System.Drawing.Color.Lime;
-            this.button_con.Location = new System.Drawing.Point(219, 11);
-            this.button_con.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_con.Location = new System.Drawing.Point(189, 14);
+            this.button_con.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.button_con.Name = "button_con";
-            this.button_con.Size = new System.Drawing.Size(170, 40);
+            this.button_con.Size = new System.Drawing.Size(139, 39);
             this.button_con.TabIndex = 1;
             this.button_con.Text = "Connect";
             this.button_con.UseVisualStyleBackColor = true;
@@ -95,10 +96,10 @@
             this.comboBox_port.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.comboBox_port.ForeColor = System.Drawing.Color.Lime;
             this.comboBox_port.FormattingEnabled = true;
-            this.comboBox_port.Location = new System.Drawing.Point(13, 15);
-            this.comboBox_port.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox_port.Location = new System.Drawing.Point(11, 14);
+            this.comboBox_port.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.comboBox_port.Name = "comboBox_port";
-            this.comboBox_port.Size = new System.Drawing.Size(200, 30);
+            this.comboBox_port.Size = new System.Drawing.Size(164, 34);
             this.comboBox_port.TabIndex = 0;
             this.comboBox_port.DropDown += new System.EventHandler(this.comboBox_port_DropDown);
             // 
@@ -106,6 +107,7 @@
             // 
             this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.splitContainer2.Name = "splitContainer2";
             this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -116,8 +118,8 @@
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.Controls.Add(this.chart1);
-            this.splitContainer2.Size = new System.Drawing.Size(1142, 657);
-            this.splitContainer2.SplitterDistance = 199;
+            this.splitContainer2.Size = new System.Drawing.Size(937, 863);
+            this.splitContainer2.SplitterDistance = 197;
             this.splitContainer2.TabIndex = 0;
             // 
             // textBox_console
@@ -128,11 +130,12 @@
             this.textBox_console.Dock = System.Windows.Forms.DockStyle.Fill;
             this.textBox_console.ForeColor = System.Drawing.Color.Lime;
             this.textBox_console.Location = new System.Drawing.Point(0, 0);
+            this.textBox_console.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.textBox_console.Multiline = true;
             this.textBox_console.Name = "textBox_console";
             this.textBox_console.ReadOnly = true;
             this.textBox_console.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox_console.Size = new System.Drawing.Size(1142, 199);
+            this.textBox_console.Size = new System.Drawing.Size(937, 197);
             this.textBox_console.TabIndex = 0;
             this.textBox_console.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -165,6 +168,7 @@
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
             this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
@@ -181,7 +185,7 @@
             this.chart1.Series.Add(series1);
             this.chart1.Series.Add(series2);
             this.chart1.Series.Add(series3);
-            this.chart1.Size = new System.Drawing.Size(1142, 454);
+            this.chart1.Size = new System.Drawing.Size(937, 662);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
@@ -192,14 +196,14 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(1142, 722);
+            this.ClientSize = new System.Drawing.Size(937, 906);
             this.Controls.Add(this.splitContainer1);
-            this.Font = new System.Drawing.Font("나눔고딕코딩", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Font = new System.Drawing.Font("D2Coding", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Margin = new System.Windows.Forms.Padding(2, 4, 2, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dust Viewer v1.2 by herz1012";
