7b17d5a2260717222c3b71acbb7ae530490006af
Galch <jaehun.han@navercorp.com>
2016-09-15 13:05:24 +0900

    SDS021 , PMS1003, 5003, 7003 supported

---
+++ DustSensorViewer/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace DustSensorViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button_con = new System.Windows.Forms.Button();
+            this.comboBox_port = new System.Windows.Forms.ComboBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.textBox_console = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button_con);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox_port);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1142, 722);
+            this.splitContainer1.SplitterDistance = 60;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button_con
+            // 
+            this.button_con.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_con.ForeColor = System.Drawing.Color.Lime;
+            this.button_con.Location = new System.Drawing.Point(219, 11);
+            this.button_con.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button_con.Name = "button_con";
+            this.button_con.Size = new System.Drawing.Size(170, 40);
+            this.button_con.TabIndex = 1;
+            this.button_con.Text = "Connect";
+            this.button_con.UseVisualStyleBackColor = true;
+            this.button_con.Click += new System.EventHandler(this.button_con_Click);
+            // 
+            // comboBox_port
+            // 
+            this.comboBox_port.BackColor = System.Drawing.Color.Black;
+            this.comboBox_port.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox_port.ForeColor = System.Drawing.Color.Lime;
+            this.comboBox_port.FormattingEnabled = true;
+            this.comboBox_port.Location = new System.Drawing.Point(13, 15);
+            this.comboBox_port.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox_port.Name = "comboBox_port";
+            this.comboBox_port.Size = new System.Drawing.Size(200, 30);
+            this.comboBox_port.TabIndex = 0;
+            this.comboBox_port.DropDown += new System.EventHandler(this.comboBox_port_DropDown);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.textBox_console);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.chart1);
+            this.splitContainer2.Size = new System.Drawing.Size(1142, 657);
+            this.splitContainer2.SplitterDistance = 199;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // textBox_console
+            // 
+            this.textBox_console.BackColor = System.Drawing.Color.Black;
+            this.textBox_console.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox_console.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox_console.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_console.ForeColor = System.Drawing.Color.Lime;
+            this.textBox_console.Location = new System.Drawing.Point(0, 0);
+            this.textBox_console.Multiline = true;
+            this.textBox_console.Name = "textBox_console";
+            this.textBox_console.ReadOnly = true;
+            this.textBox_console.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_console.Size = new System.Drawing.Size(1142, 199);
+            this.textBox_console.TabIndex = 0;
+            this.textBox_console.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Black;
+            this.chart1.BorderlineColor = System.Drawing.Color.Lime;
+            chartArea1.AxisX.LabelStyle.ForeColor = System.Drawing.Color.Lime;
+            chartArea1.AxisX.LineColor = System.Drawing.Color.Lime;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            chartArea1.AxisX.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea1.AxisX.MajorTickMark.LineColor = System.Drawing.Color.Lime;
+            chartArea1.AxisY.LabelStyle.ForeColor = System.Drawing.Color.Lime;
+            chartArea1.AxisY.LineColor = System.Drawing.Color.Lime;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.Lime;
+            chartArea1.AxisY.MajorGrid.LineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            chartArea1.AxisY.MajorTickMark.LineColor = System.Drawing.Color.Lime;
+            chartArea1.BackColor = System.Drawing.Color.Black;
+            chartArea1.BorderColor = System.Drawing.Color.Lime;
+            chartArea1.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            chartArea1.Name = "ChartArea1";
+            chartArea1.Position.Auto = false;
+            chartArea1.Position.Height = 100F;
+            chartArea1.Position.Width = 100F;
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Alignment = System.Drawing.StringAlignment.Far;
+            legend1.DockedToChartArea = "ChartArea1";
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series1.Legend = "Legend1";
+            series1.Name = "PM10";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series2.Legend = "Legend1";
+            series2.Name = "PM2.5";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastLine;
+            series3.Legend = "Legend1";
+            series3.Name = "PM1.0";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(1142, 454);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM15";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1142, 722);
+            this.Controls.Add(this.splitContainer1);
+            this.Font = new System.Drawing.Font("나눔고딕코딩", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dust Viewer v1.2 by herz1012";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button button_con;
+        private System.Windows.Forms.ComboBox comboBox_port;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TextBox textBox_console;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ DustSensorViewer/Form1.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DustSensorViewer
+{
+    public partial class Form1 : Form
+    {
+        const int CHART_AXIS_X_MAX = 300;
+        public Form1()
+        {
+            InitializeComponent();
+
+            update_comboBox();
+
+            chart1.ChartAreas[0].AxisX.Minimum = 1;
+            chart1.ChartAreas[0].AxisX.Maximum = CHART_AXIS_X_MAX + 1;
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Maximum = Double.NaN;
+        }
+
+        private void update_comboBox()
+        {
+            comboBox_port.Items.Clear();
+
+            foreach (string s in System.IO.Ports.SerialPort.GetPortNames())
+            {
+                comboBox_port.Items.Add(s);
+            }
+
+            if (comboBox_port.Items.Count > 0)
+            {
+                comboBox_port.SelectedIndex = 0;
+            }
+        }
+
+        private void comboBox_port_DropDown(object sender, EventArgs e)
+        {
+            update_comboBox();
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            Console.WriteLine("Recv packet length {0}", serialPort1.BytesToRead);
+            byte[] data = new byte[serialPort1.BytesToRead];
+            serialPort1.Read(data, 0, serialPort1.BytesToRead);
+
+            if(data.Count() > 0)
+            {
+                if (data.Length == 10)
+                    parse_SDS(data);
+                else if (data.Length == 25)
+                {
+                    //parse_PMS(data);
+                    Console.WriteLine("PMS3003 is not supoorted now");
+                }
+                else if (data.Length == 32)
+                    parse_PMS(data);
+            }
+        }
+    
+        private void button_con_Click(object sender, EventArgs e)
+        {
+            if(button_con.Text == "Connect")
+            {
+                if (!serialPort1.IsOpen)
+                {
+                    serialPort1.PortName = comboBox_port.SelectedItem.ToString();
+                    serialPort1.Open();
+                }
+                else
+                {
+                    serialPort1.Close();
+                    serialPort1.PortName = comboBox_port.SelectedItem.ToString();
+                    serialPort1.Open();
+                }
+
+                if(serialPort1.IsOpen)
+                    button_con.Text = "Disconnect";
+            } else
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                    button_con.Text = "Connect";
+                }
+            }
+        }
+
+        #region Parse and logging
+
+        bool first = true;
+        private static readonly string LOG_FILE = @"log_{0}_{1}.txt";
+        private static readonly string[] TIME_FORMAT = { "HH':'mm':'ss", "yyyy'-'MM'-'dd'\t'HH':'mm':'ss", "yyyy''MM''dd" };
+        private static readonly string[] LOG_FIRST_LINE = { "Time\tPM 10\tPM 2.5",
+                                                            "Time\tPM 10\tPM 2.5\tPM 1.0",
+                                                            "Time\tRAW 0.3\tRAW 0.5\tRAW 1.0\tRAW 2.5\tRAW 5.0\tRAW 10.0" };
+
+        private static readonly string[] LOG_LINE = { "{0}\t{1}\t{2}",
+                                                      "{0}\t{1}\t{2}\t{3}",
+                                                      "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}" };
+
+        private static readonly string[] TEXT_LINE = { "{0}\tPM10\t{1}\tPM2.5\t{2}\tSensor: {3}",
+                                                       "{0}\tPM10\t{1}\tPM2.5\t{2}\tPM1.0\t{3}\tSensor: {4}"};
+
+
+        private void update(string sensor_name, double pm10, double pm25)
+        {
+            string s_log = String.Format(LOG_LINE[0], DateTime.Now.ToString(TIME_FORMAT[0]), pm10.ToString("0.0"), pm25.ToString("0.0"));
+            string s_text = String.Format(TEXT_LINE[0], DateTime.Now.ToString(TIME_FORMAT[1]), pm10.ToString("0.0"), pm25.ToString("0.0"), sensor_name);
+
+            update_log(sensor_name, s_log);
+            update_text(s_text);
+            update_chart(pm10, pm25);
+        }
+
+        private void update(string sensor_name, int pm10, int pm25, int pm1)
+        {
+            string s_log = String.Format(LOG_LINE[1], DateTime.Now.ToString(TIME_FORMAT[0]), pm10, pm25, pm1);
+            string s_text = String.Format(TEXT_LINE[1], DateTime.Now.ToString(TIME_FORMAT[1]), pm10, pm25, pm1, sensor_name);
+            
+            update_log(sensor_name, s_log);
+            update_text(s_text);
+            update_chart(pm10, pm25, pm1);
+        }
+
+        private void update_log(string sensor_name, string msg)
+        {
+            string log_path = String.Format(LOG_FILE, sensor_name, DateTime.Now.ToString(TIME_FORMAT[2]));
+            int type = msg.Split('\t').Count();
+
+            if (first)
+            {
+                if (File.Exists(log_path))
+                {
+                    List<string> last_logs = File.ReadLines(log_path).Reverse().Take(CHART_AXIS_X_MAX).Reverse().ToList();
+
+                    foreach (var log in last_logs)
+                    {
+                        if (!LOG_FIRST_LINE.Any(log.Contains))
+                        {
+                            string[] parsed = log.Split('\t');
+                            switch (type)
+                            {
+                                case 3:
+                                    update_chart(Convert.ToDouble(parsed[1]), Convert.ToDouble(parsed[2]));
+                                    break;
+                                case 4:
+                                    update_chart(Convert.ToDouble(parsed[1]), Convert.ToDouble(parsed[2]), Convert.ToDouble(parsed[3]));
+                                    break;
+                            }
+                        }
+                    }
+                }
+                first = false;
+            }
+
+            if (!File.Exists(log_path))
+            {
+                using (StreamWriter sw = File.AppendText(log_path))
+                {                    
+                    switch(type)
+                    {
+                        case 3:
+                            sw.WriteLine(LOG_FIRST_LINE[0]);
+                            break;
+                        case 4:
+                            sw.WriteLine(LOG_FIRST_LINE[1]);
+                            break;
+                        case 7:
+                            sw.WriteLine(LOG_FIRST_LINE[2]);
+                            break;
+                    }
+                }
+            }
+
+            using (StreamWriter sw = File.AppendText(log_path))
+            {
+                sw.WriteLine(msg);
+            }
+        }
+
+        private void update_text(string s)
+        {
+            Thread thread_log = new Thread(new ThreadStart(delegate () // thread 생성
+            {
+                this.Invoke(new Action(delegate ()
+                {
+                    textBox_console.AppendText(s + "\r\n");
+                }));
+            }));
+            thread_log.Start(); // thread 실행하여 병렬작업 시작
+        }
+
+        private void update_chart<T>(T pm10, T pm25)
+        {
+            Thread thread_chart = new Thread(new ThreadStart(delegate () // thread 생성
+            {
+                this.Invoke(new Action(delegate ()
+                {
+                    if (chart1.Series["PM10"].Points.Count > chart1.ChartAreas[0].AxisX.Maximum)
+                    {
+                        chart1.Series["PM10"].Points.RemoveAt(0);
+                        chart1.Series["PM2.5"].Points.RemoveAt(0);
+                        chart1.Series["PM1.0"].Points.RemoveAt(0);
+
+                        chart1.ChartAreas[0].RecalculateAxesScale();
+                    }
+                    chart1.Series["PM10"].Points.AddY(pm10);
+                    chart1.Series["PM2.5"].Points.AddY(pm25);
+                    chart1.Series["PM1.0"].Points.AddY(0);
+
+                }));
+            }));
+            thread_chart.Start(); // thread 실행하여 병렬작업 시작
+        }
+
+        private void update_chart<T>(T pm10, T pm25, T pm1)
+        {
+            Thread thread_chart = new Thread(new ThreadStart(delegate () // thread 생성
+            {
+                this.Invoke(new Action(delegate ()
+                {
+                    if (chart1.Series["PM10"].Points.Count > chart1.ChartAreas[0].AxisX.Maximum)
+                    {
+                        chart1.Series["PM10"].Points.RemoveAt(0);
+                        chart1.Series["PM2.5"].Points.RemoveAt(0);
+                        chart1.Series["PM1.0"].Points.RemoveAt(0);
+                        chart1.ChartAreas[0].RecalculateAxesScale();
+                    }
+                    chart1.Series["PM10"].Points.AddY(pm10);
+                    chart1.Series["PM2.5"].Points.AddY(pm25);
+                    chart1.Series["PM1.0"].Points.AddY(pm1);
+
+                }));
+            }));
+            thread_chart.Start(); // thread 실행하여 병렬작업 시작
+        }
+
+
+        const byte SDS_HEADER = 0xAA;
+        private void parse_SDS(byte[] raw_input)
+        {
+            // packet format: AA C0 PM25_Low PM25_High PM10_Low PM10_High 0 0 CRC AB
+            if (raw_input.Count() != 10) return;
+            if (raw_input[0] != SDS_HEADER || raw_input[1] != 0xC0 || raw_input[9] != 0xAB) return;
+
+            byte crc = 0;
+            for(int i=0; i<6; i++)
+            {
+                crc += raw_input[i + 2];
+            }
+            if (crc != raw_input[8]) return;  // error with CRC
+
+
+            float pm25 = 0, pm10 = 0;
+
+            pm25 = (float)((int)raw_input[2] | (int)(raw_input[3] << 8)) / 10;
+            pm10 = (float)((int)raw_input[4] | (int)(raw_input[5] << 8)) / 10;
+
+            update("SDS021", pm10, pm25);
+        }
+
+        const byte PMS_HEADER = 0x42;
+        private void parse_PMS(byte[] raw_input)
+        {
+            // https://drive.google.com/file/d/0B6jowxS0fbXsQi1MSHR5UzFPa3c/view
+            // packet format: 42 4D CF_1.0_High CF_1.0_Low ...
+            if (raw_input.Count() != 32) return;
+            if (raw_input[0] != PMS_HEADER || raw_input[1] != 0x4D) return;
+
+            Func<int, int> parse = (index) =>
+            {
+                return (int)(raw_input[index] << 8) + (int)raw_input[index + 1];
+            };
+
+            int check_sum = 0;
+            for (int i = 0; i < 30; i++)
+            {
+                check_sum += raw_input[i];
+            }
+            int in_check_sum = parse(30);   // [30:31] checksum bytes
+
+            if (check_sum != in_check_sum) return;  // error with checksum
+
+            int[] CF_data = new int[3];
+            int[] Air_data = new int[3];
+            int[] raw_particle = new int[6];
+
+
+            Action<int[], int, int> parse2array = (dset, length, start_index) =>
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    dset[i] = parse(start_index + (i * 2));
+                }
+            };
+
+            parse2array(CF_data,        3, 4);
+            parse2array(Air_data,       3, 10);
+            parse2array(raw_particle,   6, 16);            
+
+            Console.WriteLine("{0}, {1},version = {2}, bug = {3}", raw_input[2], raw_input[3], raw_input[28], raw_input[29]);
+            
+            if (raw_input[28] == 114)    // PMS-5003 or 1003?
+                update("PMS5003", Air_data[2], Air_data[1], Air_data[0]);
+            if (raw_input[28] == 128)    // PMS-7003
+            {
+                update("PMS7003", Air_data[2], Air_data[1], Air_data[0]);
+
+                //string s_log = String.Format(LOG_LINE[1], DateTime.Now.ToString(TIME_FORMAT[0]), CF_data[2], CF_data[1], CF_data[0]);
+                //update_log("PMS7003_CF", s_log);
+
+                //s_log = String.Format(LOG_LINE[2], DateTime.Now.ToString(TIME_FORMAT[0]), raw_particle[0], raw_particle[1], raw_particle[2], raw_particle[3], raw_particle[4], raw_particle[5]);
+                //update_log("PMS7003_Raw", s_log);
+            }
+        }
+        #endregion
+    }
+}
