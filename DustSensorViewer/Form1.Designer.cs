d5ace33ae617e6a56a81a1cd34d4be8937c91706
Galch <jaehun.han@navercorp.com>
2016-12-13 10:16:32 +0900

    Revert "Thingspeak sample rate decrease and add exception handler."
    This reverts commit 692964d435ba80130d3e3eb3144affa62ebee41b.

--- DustSensorViewer/Form1.Designer.cs
+++ DustSensorViewer/Form1.Designer.cs
@@ -66,7 +66,7 @@
             this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
             this.splitContainer1.IsSplitterFixed = true;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(1, 4, 1, 4);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
@@ -83,8 +83,9 @@
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
-            this.splitContainer1.Size = new System.Drawing.Size(1120, 755);
+            this.splitContainer1.Size = new System.Drawing.Size(896, 604);
             this.splitContainer1.SplitterDistance = 38;
+            this.splitContainer1.SplitterWidth = 3;
             this.splitContainer1.TabIndex = 0;
             // 
             // numericUpDown_chartX
@@ -97,8 +98,8 @@
             0,
             0,
             0});
-            this.numericUpDown_chartX.Location = new System.Drawing.Point(1010, 16);
-            this.numericUpDown_chartX.Margin = new System.Windows.Forms.Padding(1);
+            this.numericUpDown_chartX.Location = new System.Drawing.Point(808, 13);
+            this.numericUpDown_chartX.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.numericUpDown_chartX.Maximum = new decimal(new int[] {
             7200,
             0,
@@ -110,7 +111,7 @@
             0,
             0});
             this.numericUpDown_chartX.Name = "numericUpDown_chartX";
-            this.numericUpDown_chartX.Size = new System.Drawing.Size(100, 25);
+            this.numericUpDown_chartX.Size = new System.Drawing.Size(80, 21);
             this.numericUpDown_chartX.TabIndex = 5;
             this.numericUpDown_chartX.Value = new decimal(new int[] {
             600,
@@ -123,10 +124,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Lime;
-            this.label1.Location = new System.Drawing.Point(949, 16);
+            this.label1.Location = new System.Drawing.Point(759, 13);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 22);
+            this.label1.Size = new System.Drawing.Size(36, 18);
             this.label1.TabIndex = 6;
             this.label1.Text = "Plot";
             // 
@@ -134,10 +135,10 @@
             // 
             this.checkBox_filter.AutoSize = true;
             this.checkBox_filter.ForeColor = System.Drawing.Color.Lime;
-            this.checkBox_filter.Location = new System.Drawing.Point(670, 16);
-            this.checkBox_filter.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox_filter.Location = new System.Drawing.Point(536, 13);
+            this.checkBox_filter.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox_filter.Name = "checkBox_filter";
-            this.checkBox_filter.Size = new System.Drawing.Size(221, 26);
+            this.checkBox_filter.Size = new System.Drawing.Size(174, 22);
             this.checkBox_filter.TabIndex = 4;
             this.checkBox_filter.Text = "Enable Moving Average";
             this.checkBox_filter.UseVisualStyleBackColor = true;
@@ -145,8 +146,8 @@
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(559, 14);
-            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(1);
+            this.numericUpDown1.Location = new System.Drawing.Point(447, 11);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             10000,
             0,
@@ -158,7 +159,7 @@
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(100, 29);
+            this.numericUpDown1.Size = new System.Drawing.Size(80, 25);
             this.numericUpDown1.TabIndex = 3;
             this.numericUpDown1.Value = new decimal(new int[] {
             30,
@@ -171,10 +172,10 @@
             // 
             this.checkBox_PMS_raw.AutoSize = true;
             this.checkBox_PMS_raw.ForeColor = System.Drawing.Color.Lime;
-            this.checkBox_PMS_raw.Location = new System.Drawing.Point(345, 16);
-            this.checkBox_PMS_raw.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox_PMS_raw.Location = new System.Drawing.Point(276, 13);
+            this.checkBox_PMS_raw.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox_PMS_raw.Name = "checkBox_PMS_raw";
-            this.checkBox_PMS_raw.Size = new System.Drawing.Size(194, 26);
+            this.checkBox_PMS_raw.Size = new System.Drawing.Size(153, 22);
             this.checkBox_PMS_raw.TabIndex = 2;
             this.checkBox_PMS_raw.Text = "Save Raw Data(PMS)";
             this.checkBox_PMS_raw.UseVisualStyleBackColor = true;
@@ -184,10 +185,10 @@
             this.button_con.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_con.Font = new System.Drawing.Font("D2Coding", 9.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.button_con.ForeColor = System.Drawing.Color.Lime;
-            this.button_con.Location = new System.Drawing.Point(158, 11);
-            this.button_con.Margin = new System.Windows.Forms.Padding(1, 4, 1, 4);
+            this.button_con.Location = new System.Drawing.Point(126, 9);
+            this.button_con.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
             this.button_con.Name = "button_con";
-            this.button_con.Size = new System.Drawing.Size(116, 32);
+            this.button_con.Size = new System.Drawing.Size(93, 26);
             this.button_con.TabIndex = 1;
             this.button_con.Text = "Connect";
             this.button_con.UseVisualStyleBackColor = true;
@@ -199,10 +200,10 @@
             this.comboBox_port.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.comboBox_port.ForeColor = System.Drawing.Color.Lime;
             this.comboBox_port.FormattingEnabled = true;
-            this.comboBox_port.Location = new System.Drawing.Point(9, 11);
-            this.comboBox_port.Margin = new System.Windows.Forms.Padding(1, 4, 1, 4);
+            this.comboBox_port.Location = new System.Drawing.Point(7, 9);
+            this.comboBox_port.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
             this.comboBox_port.Name = "comboBox_port";
-            this.comboBox_port.Size = new System.Drawing.Size(138, 29);
+            this.comboBox_port.Size = new System.Drawing.Size(111, 26);
             this.comboBox_port.TabIndex = 0;
             this.comboBox_port.DropDown += new System.EventHandler(this.comboBox_port_DropDown);
             // 
@@ -221,8 +222,9 @@
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.Controls.Add(this.chart1);
-            this.splitContainer2.Size = new System.Drawing.Size(1120, 713);
-            this.splitContainer2.SplitterDistance = 162;
+            this.splitContainer2.Size = new System.Drawing.Size(896, 563);
+            this.splitContainer2.SplitterDistance = 128;
+            this.splitContainer2.SplitterWidth = 3;
             this.splitContainer2.TabIndex = 0;
             // 
             // textBox_console
@@ -238,7 +240,7 @@
             this.textBox_console.Name = "textBox_console";
             this.textBox_console.ReadOnly = true;
             this.textBox_console.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox_console.Size = new System.Drawing.Size(1120, 162);
+            this.textBox_console.Size = new System.Drawing.Size(896, 128);
             this.textBox_console.TabIndex = 0;
             this.textBox_console.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
@@ -288,7 +290,7 @@
             this.chart1.Series.Add(series1);
             this.chart1.Series.Add(series2);
             this.chart1.Series.Add(series3);
-            this.chart1.Size = new System.Drawing.Size(1120, 547);
+            this.chart1.Size = new System.Drawing.Size(896, 432);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             // 
@@ -299,17 +301,17 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(1120, 755);
+            this.ClientSize = new System.Drawing.Size(896, 604);
             this.Controls.Add(this.splitContainer1);
             this.Font = new System.Drawing.Font("D2Coding", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(1, 4, 1, 4);
+            this.Margin = new System.Windows.Forms.Padding(1, 3, 1, 3);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Dust Viewer v1.4 by herz1012";
+            this.Text = "Dust Viewer v1.3 by herz1012";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
--- DustSensorViewer/ThingSpeakClient.cs
+++ DustSensorViewer/ThingSpeakClient.cs
@@ -9,17 +9,13 @@ namespace DustSensorViewer
 {
     class ThingSpeakClient
     {
-        private const string my_api_key = "2HJQLSHH3ZG3UGZW";
-        private const int sample_rate = 5;
-        static int sample_count = 0;
+        const string my_api_key = "2HJQLSHH3ZG3UGZW";
 
         public static async void UpdateChannelFeed(int pm10, int pm25, int pm1)
         {
-            if (sample_count++ > sample_rate)
+            using (var client = new HttpClient())
             {
-                using (var client = new HttpClient())
-                {
-                    var values = new Dictionary<string, string>
+                var values = new Dictionary<string, string>
                 {
                    { "api_key", my_api_key },
                    { "field1", pm10.ToString() },
@@ -27,48 +23,30 @@ namespace DustSensorViewer
                    { "field3", pm1.ToString() }
                 };
 
-                    try
-                    {
-                        var content = new FormUrlEncodedContent(values);
-                        var response = await client.PostAsync("https://api.thingspeak.com/update.json", content);
-                        var responseString = await response.Content.ReadAsStringAsync();
-                    }
-                    catch (Exception exception)
-                    {
-                        System.Diagnostics.Debug.WriteLine("CAUGHT EXCEPTION:");
-                        System.Diagnostics.Debug.WriteLine(exception);
-                    }
-                }
-                sample_count = 0;
+                var content = new FormUrlEncodedContent(values);
+
+                var response = await client.PostAsync("https://api.thingspeak.com/update.json", content);
+
+                var responseString = await response.Content.ReadAsStringAsync();
             }
         }
 
         public static async void UpdateChannelFeed(double pm10, double pm25)
         {
-            if (sample_count++ > sample_rate)
+            using (var client = new HttpClient())
             {
-                using (var client = new HttpClient())
+                var values = new Dictionary<string, string>
                 {
-                    var values = new Dictionary<string, string>
-                    {
-                       { "api_key", my_api_key },
-                       { "field1", pm10.ToString("0.0") },
-                       { "field2", pm25.ToString("0.0") }
-                    };
+                   { "api_key", my_api_key },
+                   { "field1", pm10.ToString("0.0") },
+                   { "field2", pm25.ToString("0.0") }
+                };
 
-                    try
-                    {
-                        var content = new FormUrlEncodedContent(values);
-                        var response = await client.PostAsync("https://api.thingspeak.com/update.json", content);
-                        var responseString = await response.Content.ReadAsStringAsync();
-                    }
-                    catch (Exception exception)
-                    {
-                        System.Diagnostics.Debug.WriteLine("CAUGHT EXCEPTION:");
-                        System.Diagnostics.Debug.WriteLine(exception);
-                    }
-                }
-                sample_count = 0;
+                var content = new FormUrlEncodedContent(values);
+
+                var response = await client.PostAsync("https://api.thingspeak.com/update.json", content);
+
+                var responseString = await response.Content.ReadAsStringAsync();
             }
         }
     }
